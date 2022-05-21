19e544b6efd31d84beddafd5117fd615f2702479
Tincho <69773576+tinroldan@users.noreply.github.com>
2022-05-20 20:13:20 -0500

    init

---
+++ Assets/Finales/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Transform ballSpawn;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/axistest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class axistest : MonoBehaviour
+{
+    public float axist;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        axist = Input.GetAxis("Tilia.Input.UnityInputManager_Axis9"); ;
+    }
+}
